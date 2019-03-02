fd1c9b33fdf6aa0c6088e48a5ef1d42a41973934
FlaxBear <ht14a032@oecu.jp>
2019-03-02 12:32:29 +0900

    Update what is begin created

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # BookMarkGroup
+====
+
 Share Web bookmarks within group
+
+## Description
+When you want to share bookmarks and favorites in the browser with workplaces, teams, friends, etc.,
+you can send a URL by e-mail or share it without showing the screen by using WebBookMark.
+
+## Demo
+
+## VS. 
+
+## Requirement
+
+## Usage
+
+## Install
+
+## Contribution
+
+## Licence
+
+[MIT](https://github.com/tcnksm/tool/blob/master/LICENCE)
+
+## Authors
+[FlaxBear](https://github.com/FlaxBear)
+[FlaxBear]E-mail(flaxbear.github@gmail.com)
---
+++ plugin/ver1_0/chorme/BookMarkGroup/js/mybookmark.js
@@ -0,0 +1,179 @@
+var localStorage_key = "BookMarkGroup";
+
+// Function: button_processing
+// Description: Perform processing of each button
+// Input: [String] mode: Number allocated to each button
+// Output: None
+function button_processing(mode) {
+	switch(mode) {
+		case '2':
+			//createGroup(); // ok
+			updateGroup();
+		break;
+		case '6':
+			removeGroup();
+		break;
+		default:
+	}
+	return;
+}
+
+// Function: callBack_pass
+// Description: End callback without doing anything
+// Input: None
+// Output: None
+function callBack_pass() {return false;}
+
+// Function: createCreateDetailsObject
+// Description: Create the DetailsObject type needed to create a new bookmark or folder property
+// Input: [String] parentId: Parent directory id
+//		  [Int] index: Order num
+//		  [String] title: Name of bookmark or folder
+//		  [String] url: url
+// Output: [DetailsObject型] obj
+function createCreateDetailsObject(parentId, index, title, url)
+{
+	var obj = {
+		parentId: parentId,
+		index: index,
+		title: title,
+		url: url,
+	}
+	return obj;
+}
+
+// Function: writeLocalStorage
+// Description: Save data to localStorage
+// Input: [JSON] storage_data
+// Output: None
+function writeLocalStorage(storage_data) 
+{
+	localStorage.setItem(localStorage_key, storage_data);
+	return;
+}
+
+// Function: loadLocalStorage
+// Description: Extract data to localStorage
+// Input: None
+// Output: [JSON] storage_data
+function loadLocalStorage() 
+{
+	var storage_data = localStorage.getItem(localStorage_key);
+	return storage_data;
+}
+
+// Function: createGroup
+// Description: Create a GroupFolder(See callBack_createGroup)
+// Input: None
+// Output: None
+function createGroup() {
+	// make_bookmark_groupのテキストボックスの中身を見て、空でなければ作成を行う
+	if(document.main.make_bookmark_group.value != '') {
+		chrome.bookmarks.getChildren('1', callBack_createGroup);
+	} else {
+		// エラーメッセージを表示（未作成）
+	}
+	return;
+}
+
+// 関数: callBack_reateGroup
+// 説明: ブックマークの一覧を取得し、MyBookMarkフォルダーがなければ作成し、グループフォルダーの作成を行う
+// 入力: BookmarkTreeNode型(リスト型) bookmarkItems ブックマークに入っている各フォルダーの情報リスト(コールバック関数の呼び出し時取得)
+// 出力: なし
+function callBack_createGroup(bookmarkItems) {
+	// mybookmarkフォルダーを新規で作成するかの判定用
+	var folder_flag = false;
+
+	for(item of bookmarkItems) 
+	{
+		if(item.title == 'MyBookMark' && item.url == undefined) 
+		{
+			folder_flag = true;
+			//グループファイルを作成する
+			var group_folder_title = document.main.make_bookmark_group.value;
+			var mybookmark = createCreateDetailsObject(item.id, 0, group_folder_title, null);
+			chrome.bookmarks.create(mybookmark, callBack_pass);
+		}
+	}
+	if(folder_flag == false) 
+	{
+		// 新規でMyBookMarkフォルダーを作成を行い、グループファイルを作成する
+		var mybookmark = createCreateDetailsObject('1', 0, 'MyBookMark', null);
+		chrome.bookmarks.create(mybookmark, callBack_pass);
+		// 回帰的に再び検索を行い、グループフォルダーを作成される
+		chrome.bookmarks.getChildren('1', callBack_reateGroup);
+	}
+}
+
+// 関数: updateGroup
+// 説明: LocalStorageにデータを更新を行う、
+//からデータを取得し、
+// 入力: なし
+// 出力: なし
+function updateGroup()
+{
+	var localstorage = loadLocalStorage();
+	if(localstorage != null) {
+		if(localstorage.mybookmark.folder_id != "") {
+			// lovalstoregeの更新
+		} else {
+			
+		}
+	} else {
+
+	}
+}
+
+// 関数: removeGroup
+// 説明: グループフォルダーの作成を行う(詳細はcallBack_removeGroup参照)
+// 入力: なし
+// 出力: なし
+function removeGroup()
+{
+	// make_bookmark_groupのテキストボックスの中身を見て、空でなければ削除を行う
+	if(document.main.make_bookmark_group.value != '') {
+		chrome.bookmarks.getChildren('1', callBack_removeGroup);
+	} else {
+		// エラーメッセージを表示（未作成）
+	}
+	return;
+}
+
+// 関数: callBack_removeGroup
+// 説明: ブックマークの一覧を取得し、MyBookMarkフォルダーがなければエラーメッセージを表示し、存在するならばグループフォルダーの削除を行う
+// 入力: BookmarkTreeNode型(リスト型) bookmarkItems ブックマークに入っている各フォルダーの情報リスト(コールバック関数の呼び出し時取得)
+// 出力: なし
+function callBack_removeGroup(bookmarkItems) {
+	var remove_flag = false;
+	for(item of bookmarkItems) 
+	{
+		if(item.title == 'MyBookMark' && item.url == undefined) 
+		{
+			//グループファイルを削除する
+			var group_folder_id = document.main.remove_bookmark_group.value;
+			chrome.bookmarks.remove(group_folder_id, callBack_pass)
+			remove_flag = true;
+		}
+	}
+	if(remove_flag == true){
+		// 削除メッセージの表示
+	} else {
+		// エラーメッセージを表示
+	}
+}
+
+
+function callBack_writeLocalStorage(bookmarkItem)
+{
+	// var key = "MyBookMark";
+	// var storage_data = localStorage.getItem(key);
+
+	console.log(bookmarkItem.id);
+	var test = document.getElementById("debug_result");
+	test.innerHTML = bookmarkItem.id;
+
+	// var test = document.getElementById("debug_result");
+	// test.innerHTML = bookmarkItems[0].id;
+
+	// localStorage.setItem(key, bookmarkItems);
+}
